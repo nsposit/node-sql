c998819601bc8bb614e40a4da8945d2536ffae26
Nate Sposit <nathansposit@gmail.com>
2022-02-16 21:35:59 -0500

    adds postgres test

--- test/postgres.spec-.js
+++ test/postgres.spec-.js
@@ -11,44 +11,26 @@ const db = new sql.Database(
   { ssl: false }
 );
 
-
-
 const tbl = db.table("member_tbl", {
   id: {
-    type: "int",
+    type: "serial",
   },
   primary_email: {
     type: "varchar(75)",
   },
   verified_email: {
-    type: "tinyint(1)",
+    type: "boolean",
   },
   last_name: {
     type: "varchar(40)",
   },
 });
 
-//create table
-// const tbl = new sql.Table(db, "member_tbl", {
-//   id: {
-//     type: "int",
-//   },
-//   primary_email: {
-//     type: "varchar(75)",
-//   },
-//   verified_email: {
-//     type: "tinyint(1)",
-//   },
-//   last_name: {
-//     type: "varchar(40)",
-//   },
-// });
 
 // init test users
 const timestamp = Date.now();
 let user1 = { id: 1, primary_email: `testuser${timestamp}@mail1.mil` };
 let user2 = { id: 2, primary_email: `testuser${timestamp}@mail2.mil` };
-let user3 = { id: 3, primary_email: `testuser${timestamp}@mail3.mil` };
 
 describe("core", () => {
 
@@ -56,26 +38,21 @@ describe("core", () => {
     await db.query(`CREATE SCHEMA IF NOT EXISTS test;`);
 
     await db.query(`CREATE TABLE IF NOT EXISTS test.member_tbl (
-      id INT AUTO_INCREMENT,
+      id serial PRIMARY KEY,
       primary_email VARCHAR(75),
-      verified_email TINYINT(1) DEFAULT 0,
-      last_name VARCHAR(40),
-      PRIMARY KEY (id)
+      verified_email BOOLEAN DEFAULT false,
+      last_name VARCHAR(40)
     );`);
   });
 
   test("insert", async () => {
-    try {
-      const { id } = await tbl.insert({ primary_email: user1.primary_email });
-      user1.id = id;
-      expect(id).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+      const rows = await tbl.insert({ primary_email: user1.primary_email });
+      user1.id = rows[0].id;
+      expect(rows[0].id).toBeTruthy();
   });
 
   test("select", async () => {
-    try {
+    
       const rows = await tbl.select(
         ["primary_email"],
         [
@@ -86,68 +63,55 @@ describe("core", () => {
         ]
       );
       expect(rows[0].primary_email).toEqual(user1.primary_email);
-    } catch (ex) {
-      throw ex;
-    }
+    
   });
 
   test("update", async () => {
-    try {
-      const result = await tbl.update({ verified_email: 1 }, [
+    
+      const rows = await tbl.update({ verified_email: true }, [
         { key: "id", value: user1.id },
       ]);
-      expect(result).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+      console.log(rows);
+      expect(rows[0].verified_email).toBeTruthy();
+    
   });
 
   test("delete", async () => {
-    try {
+    
       const result = await tbl.delete([{ key: "id", value: user1.id }]);
       expect(result).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+    
   });
 });
 
 describe("singletons", () => {
   test("insert", async () => {
-    try {
-      const { id } = await tbl.insert({ primary_email: user2.primary_email });
-      user2.id = id;
-      expect(id).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+    
+      const rows = await tbl.insert({ primary_email: user2.primary_email });
+      user2.id = rows[0].id;
+      expect(rows[0].id).toBeTruthy();
+    
   });
 
   test("selectOne", async () => {
-    try {
-      const row = await tbl.selectOne(user2.id, ["primary_email"]);
-      expect(row.primary_email).toEqual(user2.primary_email);
-    } catch (ex) {
-      throw ex;
-    }
+    
+      const rows = await tbl.selectOne(user2.id, ["primary_email"]);
+      expect(rows[0].primary_email).toEqual(user2.primary_email);
+    
   });
 
   test("updateOne", async () => {
-    try {
-      const result = await tbl.updateOne(user2.id, { verified_email: 1 });
-      expect(result).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+    
+      const rows = await tbl.updateOne(user2.id, { verified_email: true });
+      expect(rows[0].verified_email).toBeTruthy();
+    
   });
 
   test("deleteOne", async () => {
-    try {
+    
       const result = await tbl.deleteOne(user2.id);
       expect(result).toBeTruthy();
-    } catch (ex) {
-      throw ex;
-    }
+    
   });
 
   afterAll(async () => {
